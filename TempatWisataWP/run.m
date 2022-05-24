6fb64cbf4c90247cdc441d5c4826109bcd05ad6b
hammam06 <75723893+hammam06@users.noreply.github.com>
2022-05-24 16:21:06 +0700

    Delete TempatWisataWP directory

--- TempatWisataWP/run.m
+++
@@ -1,5 +0,0 @@
-valueCategory = ["Bahari" 10; "Budaya" 20; "Cagar Alam" 30; "Pusat Perbelanjaan" 25; "Taman Hiburan" 15; "Tempat Ibadah" 50];
-cityPicked = ["Semarang"];
-weightCriteria = [4 2 9];
-
-main(valueCategory, cityPicked, weightCriteria)
